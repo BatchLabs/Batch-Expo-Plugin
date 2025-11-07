4615afad990feab69b276b0718402ca587258998
Arnaud Roland <arnaud.roland@batch.com>
2025-11-07 17:10:59 +0100

    android: fix wrong batch module name

--- android/src/main/java/expo/modules/batch/BatchExpoApplicationLifecycleListener.kt
+++ android/src/main/java/expo/modules/batch/BatchExpoApplicationLifecycleListener.kt
@@ -2,12 +2,12 @@ package expo.modules.batch
 
 import android.app.Application
 import android.util.Log
-import com.batch.batch_rn.RNBatchModuleImpl
+import com.batch.batch_rn.RNBatchModule
 import expo.modules.core.interfaces.ApplicationLifecycleListener
 
 class BatchExpoApplicationLifecycleListener : ApplicationLifecycleListener {
 
     override fun onCreate(application: Application) {
-        RNBatchModuleImpl.initialize(application);
+        RNBatchModule.initialize(application);
     }
 }
